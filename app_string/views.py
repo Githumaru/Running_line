07d8906a05768703fa635b15d90a9b9d02b03c3d
Githumaru <123551302+Githumaru@users.noreply.github.com>
2023-06-02 10:37:08 +0300

    Update views.py

--- app_string/views.py
+++ app_string/views.py
@@ -10,40 +10,46 @@ from pymongo import MongoClient
 
 
 def my_video_view(request):
+    # Set video parameters
     width, height = 100, 100
     duration = 3
     background_color = (200, 100, 0)
     text_color = (0, 200, 100)
-
+    
+    # Create a canvas with the specified parameters
     canvas = Image.new('RGB', (width, height), background_color)
-
-    text = request.GET.get('text', '')  # Получаем текст из GET-параметра запроса
+    
+    # Get the text from the GET request parameter
+    text = request.GET.get('text', '') 
     k = len(text) * 8
     fps = len(text) * 3
-
+    
+    # Load the font
     fontpath = "././ARIAL.ttf"
     font = ImageFont.truetype(fontpath, 20)
-
+    
+    # Get data from the POST request parameters
     title = request.POST.get('title')
     description = request.POST.get('description')
 
-
+    # Connect to MongoDB
     client = MongoClient('mongodb://localhost:27017/')
-
     db = client['datatest']
-
     collection = db['videodata']
 
-    # Создание и сохранение экземпляра модели VideoRequest
+    # Create and save an instance of the VideoRequest model
     video_request = VideoRequest(title=title, description=description)
     video_request.save()
-
+    
+    # Set the request time and text for the video
     video_request.request_time = timezone.now()
     video_request.text = text
     video_request.save()
-
+    
+    # Create a video writer object
     video = cv2.VideoWriter('running_line.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
-
+    
+    # Generate each frame of the video
     for t in np.linspace(0, duration, int(duration * fps), endpoint=False):
         frame = np.copy(canvas)
         start_x = int(t * (-k) / duration) + 20
@@ -55,16 +61,16 @@ def my_video_view(request):
         video.write(frame)
 
     video.release()
-
+    # Create a document with the request data
     document = {
         'created_at': video_request.created_at,
         'request_time': video_request.created_at,
         'text': text
     }
-
+    # Insert the document into the MongoDB collection
     collection.insert_one(document)
 
-    # Читаем сгенерированное видео и возвращаем его в HTTP-ответе
+    # Read the generated video and return it in the HTTP response
     with open('running_line.mp4', 'rb') as f:
         response = HttpResponse(f.read(), content_type='video/mp4')
         response['Content-Disposition'] = 'attachment; filename=running_line.mp4'
