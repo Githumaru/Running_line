0fb2dad3af366bd1c77c9ae2470a325a2c213e48
Githumaru <123551302+Githumaru@users.noreply.github.com>
2023-06-02 10:39:05 +0300

    Update models.py

--- app_string/models.py
+++ app_string/models.py
@@ -5,8 +5,8 @@ class VideoRequest(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    request_time = models.DateTimeField(default=timezone.now)  # Поле для хранения времени запроса
-    text = models.CharField(max_length=100)
+    request_time = models.DateTimeField(default=timezone.now)  # Field to store the request time
+    text = models.CharField(max_length=100) # Field to store the text of line
 
     def __str__(self):
         return self.title
